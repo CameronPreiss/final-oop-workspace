b75d31b66977ff954b0ee169bc4ae120f25fb969
CameronPreiss <cameron.preiss@student.adelaide.edu.au>
2024-10-22 12:04:52 +1030

    fdsfsdjf

--- main-2.cpp
+++ main-2.cpp
@@ -7,11 +7,8 @@ int main(){
     Ship a(1,2);
     Mine b(4,6);
 
-    a.move(2,3);
-    b.explode();
-
-    Explosion c(3,3);
+    Explosion c = b.explode();
     c.apply(a);
 
-    std::cout << c.getType() << std::endl;
+    std::cout << a.getType() << std::endl;
 }
