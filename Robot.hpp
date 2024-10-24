f3efd535aed1a312fda9a68b860cea0e0e40dc08
CameronPreiss <cameron.preiss@student.adelaide.edu.au>
2024-10-24 16:25:08 +1030

    f

--- Robot.hpp
+++ Robot.hpp
@@ -21,12 +21,13 @@ class Robot : public GridItem{
         }
         bool move(int xOffset, int yOffset){
             if (xOffset == 0 || yOffset == 0){
-                if (std::get<0>(coordinates)+xOffset <= width || std::get<0>(coordinates)+xOffset >= 0 || std::get<1>(coordinates)+yOffset <= height || std::get<1>(coordinates)+yOffset >= 0){
                 int x = std::get<0>(coordinates) + xOffset;
                 int y = std::get<1>(coordinates) + yOffset;
+                if (x > gridWidth || x <0 || y >gridHeight || y<0){
+                    return false;
+                }
                 coordinates = std::make_pair(x,y);
                 return true;
-                }
             } else {
                 return false;
             }
